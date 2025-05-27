c5b7d9833c6c5919f790d2d1f9390b384d4153dd
Scuzz50 <ktheos@scuzz.ca>
2025-05-26 20:49:46 -0400

    Add files via upload

--- userspace/src/main.rs
+++ userspace/src/main.rs
@@ -1,12 +1,16 @@
-use aya::{include_bytes_aligned, Ebpf};
+use aya::Ebpf;
 use aya::programs::Xdp;
 use anyhow::{Context, Result};
 use std::env;
+use std::path::PathBuf;
 
 fn main() -> Result<()> {
     let iface = env::args().nth(1).context("No interface given")?;
-    let mut bpf = Ebpf::load_file("target/ferros_firewall_ebpf.o")
-        .context("Loading eBPF object file")?;
+
+    let mut elf_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    elf_path.push("../target/ferros_firewall_ebpf.o");
+
+    let mut bpf = Ebpf::load_file(&elf_path).context("Loading eBPF object file")?;
 
     let program: &mut Xdp = bpf.program_mut("firewall").unwrap().try_into()?;
     program.load()?;

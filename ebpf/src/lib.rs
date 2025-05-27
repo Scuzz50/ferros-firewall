b6d6a32cbd37f3d7e8d1189601dc79d2b89fb4cb
Scuzz50 <ktheos@scuzz.ca>
2025-05-26 20:23:41 -0400

    Add files via upload

--- ebpf/src/lib.rs
+++ ebpf/src/lib.rs
@@ -11,6 +11,8 @@ use aya_ebpf::{
 #[map(name = "BLOCKED_IPS")]
 static mut BLOCKED_IPS: HashMap<u32, u8> = HashMap::<u32, u8>::with_max_entries(1024, 0);
 
+#[no_mangle]
+#[used]
 #[xdp]
 pub fn firewall(ctx: XdpContext) -> u32 {
     match try_firewall(ctx) {

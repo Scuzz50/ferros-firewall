8a2fe515023d37393b5c2e7e89736c347f13cae3
Scuzz50 <ktheos@scuzz.ca>
2025-05-26 20:45:49 -0400

    Add files via upload

--- userspace/src/main.rs
+++ userspace/src/main.rs
@@ -1,17 +1,17 @@
+use aya::{include_bytes_aligned, Ebpf};
 use aya::programs::Xdp;
-use aya::Bpf;
-use std::convert::TryInto;
+use anyhow::{Context, Result};
 use std::env;
-use anyhow::Context;
 
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    env_logger::init();
-    let iface = env::args().nth(1).unwrap_or_else(|| "eth0".to_string());
-    let mut bpf = Bpf::load_file("../target/ferros_firewall_ebpf.o").context("Loading eBPF object file")?;
+fn main() -> Result<()> {
+    let iface = env::args().nth(1).context("No interface given")?;
+    let mut bpf = Ebpf::load_file("target/ferros_firewall_ebpf.o")
+        .context("Loading eBPF object file")?;
+
     let program: &mut Xdp = bpf.program_mut("firewall").unwrap().try_into()?;
     program.load()?;
     program.attach(&iface, aya::programs::XdpFlags::default())?;
-    println!("✅ eBPF program attached to {}", iface);
+
+    println!("✅ eBPF firewall attached to {}", iface);
     Ok(())
 }
